a42bbea6a70b959d67fbfc6c4fa39559a4611ec1
Gabriele Coquillard <gabriele.digival@gmail.com>
2018-01-04 19:45:21 +0100

    2.2.7 bindings

--- RecyclerView-Animators/Properties/AssemblyInfo.cs
+++ RecyclerView-Animators/Properties/AssemblyInfo.cs
@@ -1,17 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using Android.App;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RecyclerView_Animators")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("which provides simple Item animations to RecyclerView items")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Gabriele Coquillard, Wasabeef")]
 [assembly: AssemblyProduct("RecyclerView-Animators")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Gabriele Coquillard 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
@@ -26,5 +24,5 @@ using Android.App;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.2.7")]
+[assembly: AssemblyFileVersion("2.2.7")]
